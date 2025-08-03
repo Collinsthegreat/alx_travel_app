ac8a81c45ff2b7e29f25bb30272de2ff140bdc03
Abuchi Nwajagu Collins <76597982+Collinsthegreat@users.noreply.github.com>
2025-08-03 21:43:37 +0100

    Update urls.py

--- alx_travel_app/alx_travel_app/listings/urls.py
+++ alx_travel_app/alx_travel_app/listings/urls.py
@@ -1,5 +1,23 @@
 # alx_travel_app/listings/urls.py
 
-from django.urls import path
+# listings/urls.py
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ListingViewSet, BookingViewSet
 
-urlpatterns = []
+router = DefaultRouter()
+router.register(r'listings', ListingViewSet)
+router.register(r'bookings', BookingViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
+
+# alx_travel_app/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('listings.urls')),  # 👈 Adds API routes under /api/
+]
